46648ba6e00c5f8bdfa8ec321768d38ba655e697
corche-2000 <68491019+corche-2000@users.noreply.github.com>
2020-11-27 11:24:35 +0900

    Update example1.py

--- example1.py
+++ example1.py
@@ -17,5 +17,10 @@ async def on_message(message):
         return None
     if message.content == "고마워":
         await message.channel.send("JJH봇을 써주셔서 고마워여!!!")
+    if message.content=="help" or message.content.startswith("도움말"):
+        embed = discord.embed(title = "명령어", description = "** **", color=0xd1173b)
+        embed.add_field(name = "청소 n", value = "적은 수 만큼 채팅을 삭제 할 수 있어여![메시지 관리권한 이상만 가능해여!]
+        await message.channel.send(embed=embed)
+    
 
 app.run("token")
